bfb343ab58dfa99890d3b12308806e98e2a77d88
Andy <andy.harney@andyharney.co.uk>
2014-02-03 12:32:22 +0000

    Changed Vars, Added Comments, Updated Slightly
    New user defined sleep interval,, updated time handling.

--- pyResolve.py
+++ pyResolve.py
@@ -12,21 +12,29 @@
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
 
+# Define the function
 
 def resolver():
+
+
+    # Import the modules
     import os.path
     from urllib import request
-    from time import gmtime, strftime, sleep
+    from time import gmtime, strftime, sleep, time
     from random import choice
+
+    # Grab some info from the user
     while True:
         try:
             usr_log = int(input('Log length in days? - '))
             max_logs = int(input('How many logs do you wish to create? - '))
+            sleep_timer = int(input('In seconds, how long do you wish to wait between checks? - '))
             break
         except ValueError:
             print('Hmmm.... please only enter whole numbers. E.g. 1, 2, 3 Not. 2.5, 3.3')
             continue
-    pingservs = ['http://74.125.228.100',
+    # Define the list of addresses (IPs) to resolve
+    res_serv = ['http://74.125.228.100',
                  'http://74.125.224.72/',
                  'http://173.194.115.23',
                  'http://173.194.115.24',
@@ -36,31 +44,47 @@ def resolver():
                  'http://173.194.64.160',
                  'http://134.170.188.84',
                  'http://87.98.168.164']
-
-    curtime = strftime("%Y-%m-%d %H-%M-%S", gmtime())
+    # Set a few vars, day or log_length not elegant
     logs = 0
     mins = 0
     day = 1440
     log_length = day * usr_log
+    # Initial print to let the user know its running
+    print('Running.....')
+    start_time = time()
     while logs < max_logs:
         logs += 1
+        # Current time is taken and logged into the filename
+        # This avoids overwriting logs with new ones
+        curtime = strftime("%Y-%m-%d %H-%M-%S", gmtime())
         outlog = os.path.expanduser('ping_' + curtime + '.log')
         outfile = open(outlog, 'w')
         outfile.close()
         while mins < log_length:
             mins += 1
+            # A failure to resolve with urlopen throws an exception, we catch it
             try:
-                randserv = choice(pingservs)
-                request.urlopen(randserv, timeout=2)
+                # Here we pick a server to resolve from res_servs
+                # If it resolves the time and url are logged
+                randserv = choice(res_serv)
+                request.urlopen(ranserv, timeout=2)
                 outfile = open(outlog, 'a')
                 outfile.write(curtime + ' - Resolves - ' + randserv + '\n')
                 outfile.close()
+            # Catch the unresolved url and log the url and time
+            # Who knows the url might actually be down
             except:
                 outfile = open(outlog, 'a')
                 outfile.write(curtime + ' - No Connection - ' + randserv + '\n')
                 outfile.close()
-            sleep(60)
-            curtime = strftime("%Y-%m-%d %H-%M-%S", gmtime())
-            print('Running for ' + str(mins) + ' minutes', end='\r')
+            # Now we wait
+            sleep(sleep_timer)
+            run_time = time() - start_time
+            if run_time > 60:
+                minutes, seconds = divmod(run_time, 60)
+                print('Running for ' + str(int(minutes)) + ' minutes ' + str(int(seconds)) + ' seconds', end = '\r')
+            else:
+                seconds = time() - start_time
+                print('Running for ' + str(int(seconds)) + ' seconds', end = '\r')
 
 resolver()
